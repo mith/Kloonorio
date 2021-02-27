cc57ccf88ab3df4d0d339a0d9681815178752487
Simon Voordouw <simonvoordouw@gmail.com>
2021-02-28 00:12:25 +0100

    basic picking from tilemap

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,16 @@
 use bevy::input::mouse::MouseMotion;
-use bevy::{asset::LoadState, prelude::*, sprite::TextureAtlasBuilder};
+use bevy::{asset::LoadState, prelude::*, render::camera::Camera, sprite::TextureAtlasBuilder};
 use bevy_tilemap::prelude::*;
-use tracing::{info, Level};
-use tracing_subscriber::FmtSubscriber;
 
 fn main() {
-        App::build()
-        .add_resource(Msaa { samples: 4 })
+    App::build()
         .init_resource::<SpriteHandles>()
         .init_resource::<GameState>()
+        .init_resource::<CursorState>()
         .add_plugins(DefaultPlugins)
         .add_plugins(TilemapDefaultPlugins)
         .add_startup_system(setup.system())
-        .add_system(camera_movement_system.system())
+        .add_system(mouse_world_interaction_system.system())
         .add_system(keyboard_input_system.system())
         .add_system(load.system())
         .add_system(build_world.system())
@@ -26,8 +24,13 @@ struct SpriteHandles {
 }
 
 #[derive(Default)]
-struct GameState {
+struct CursorState {
     mouse_motion_event_reader: EventReader<MouseMotion>,
+    under_cursor: Option<usize>,
+}
+
+#[derive(Default)]
+struct GameState {
     map_loaded: bool,
     spawned: bool,
 }
@@ -85,10 +88,24 @@ fn load(
         let tilemap = Tilemap::builder()
             .auto_chunk()
             .topology(GridTopology::Square)
+            .chunk_dimensions(32, 32)
+            .auto_spawn(3, 3)
             .tile_dimensions(16, 16)
             .z_layers(2)
-            .add_layer(LayerKind::Dense, 0)
-            .add_layer(LayerKind::Sparse, 1)
+            .add_layer(
+                TilemapLayer {
+                    kind: LayerKind::Dense,
+                    ..Default::default()
+                },
+                0,
+            )
+            .add_layer(
+                TilemapLayer {
+                    kind: LayerKind::Sparse,
+                    ..Default::default()
+                },
+                1,
+            )
             .texture_atlas(atlas_handle)
             .finish()
             .unwrap();
@@ -125,19 +142,37 @@ fn build_world(
         let floor_index = texture_atlas.get_texture_index(&floor).unwrap();
         let coal_index = texture_atlas.get_texture_index(&coal).unwrap();
 
-        map.insert_chunk((0, 0)).unwrap();
-
         let mut tiles = Vec::new();
         for y in -100..100 {
             for x in -100..100 {
-                let tile = Tile::new((x, y), floor_index);
+                let tile = Tile {
+                    point: (x, y),
+                    sprite_index: floor_index,
+                    z_order: 0,
+                    ..Default::default()
+                };
                 tiles.push(tile);
             }
         }
 
-        tiles.push(Tile::with_z_order((2, 4), coal_index, 1));
-        tiles.push(Tile::with_z_order((0, 0), coal_index, 1));
-        tiles.push(Tile::with_z_order((10, 10), coal_index, 1));
+        tiles.push(Tile {
+            point: (2, 4),
+            sprite_index: coal_index,
+            z_order: 1,
+            ..Default::default()
+        });
+        tiles.push(Tile {
+            point: (0, 0),
+            sprite_index: coal_index,
+            z_order: 1,
+            ..Default::default()
+        });
+        tiles.push(Tile {
+            point: (10, 10),
+            sprite_index: coal_index,
+            z_order: 1,
+            ..Default::default()
+        });
         map.insert_tiles(tiles).unwrap();
 
         let load_chunks = 4;
@@ -153,10 +188,50 @@ fn build_world(
     }
 }
 
-fn camera_movement_system(mut state: Local<GameState>, mouse_motion_events: Res<Events<MouseMotion>>) {
-    for event in state.mouse_motion_event_reader.iter(&mouse_motion_events) {
-        trace!("{:?}", event);
+fn mouse_world_interaction_system(
+    mut state: Local<CursorState>,
+    windows: Res<Windows>,
+    mouse_button_input: Res<Input<MouseButton>>,
+    camera_transforms: Query<&GlobalTransform, With<Camera>>,
+    mut tilemap_query: Query<(&mut Tilemap, &GlobalTransform)>,
+) {
+    let maybe_window: Option<Vec3> = windows.get_primary().and_then(|window| {
+        window.cursor_position().map(|cursor_position| {
+            Vec3::new(cursor_position.x - window.width() / 2.0, cursor_position.y - window.height() / 2.0, 0.0)
+        })
+    });
+    let cursor_position = if let Some(window) = maybe_window {
+        window
+    } else {
+        return;
+    };
+
+    for camera_transform in camera_transforms.iter() {
+        for (mut tilemap, tilemap_transform) in tilemap_query.iter_mut() {
+            if mouse_button_input.just_pressed(MouseButton::Right) {
+                let tile_position = get_tile_position_under_cursor(
+                    cursor_position,
+                    camera_transform,
+                    tilemap_transform,
+                    tilemap.tile_width(),
+                );
+                let tile = tilemap.get_tile(tile_position, 1);
+                state.under_cursor = tile.map(|tile| tile.index);
+            }
+        }
     }
+}
+
+fn get_tile_position_under_cursor(
+    cursor_position: Vec3,
+    camera_transform: &GlobalTransform,
+    tilemap_transform: &GlobalTransform,
+    tile_size: u32,
+) -> (i32, i32) {
+    let translation = (camera_transform.mul_vec3(cursor_position)) - tilemap_transform.translation;
+    let point_x = translation.x / tile_size as f32;
+    let point_y = translation.y / tile_size as f32;
+    return (point_x.floor() as i32, point_y.floor() as i32);
 }
 
 fn keyboard_input_system(
@@ -165,24 +240,43 @@ fn keyboard_input_system(
 ) {
     let mut direction = Vec3::new(0.0, 0.0, 0.0);
     if keyboard_input.pressed(KeyCode::A) {
-            direction.x = 1.0
+        direction.x = 1.0
     }
 
     if keyboard_input.pressed(KeyCode::E) {
-            direction.x = -1.0
+        direction.x = -1.0
     }
 
     if keyboard_input.pressed(KeyCode::Comma) {
-            direction.y = -1.0
+        direction.y = -1.0
     }
 
     if keyboard_input.pressed(KeyCode::O) {
-            direction.y = 1.0
+        direction.y = 1.0
     }
 
     if direction.length_squared() > 0.0 {
         for mut transform in player_query.iter_mut() {
             transform.translation = transform.translation - direction.normalize() * 8.0;
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_get_tile_position_under_cursor() {
+        let cursor_position = Vec3::new(3.0, 4.0, 0.0);
+        let camera_transform = Transform::from_translation(-cursor_position);
+        let tilemap_transform = Transform::identity();
+        let tile_size = 16;
+        let tile_position = get_tile_position_under_cursor(
+            cursor_position,
+            &camera_transform,
+            &tilemap_transform,
+            tile_size,
+        );
+        assert_eq!((0, 0), tile_position);
     }
 }
