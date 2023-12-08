3e780e884b4d46987330639051bf7098a20bb6ad
Simon Voordouw <simonvoordouw@gmail.com>
2023-12-08 19:49:53 +0100

    Show crafting progress

--- src/ui/crafting_queue_ui.rs
+++ src/ui/crafting_queue_ui.rs
@@ -3,7 +3,7 @@ use bevy::ecs::{
     system::{Query, Res},
 };
 use bevy_egui::EguiContexts;
-use egui::{Align2, PointerButton, Sense};
+use egui::{Align2, Color32, PointerButton, Response, Sense};
 
 use crate::{loading::Icons, player::Player, types::CraftingQueue};
 
@@ -23,16 +23,7 @@ pub fn crafting_queue_ui(
             for mut crafting_queue in &mut crafting_queue_query {
                 ui.horizontal(|ui| {
                     for (index, build) in crafting_queue.0.iter_mut().enumerate() {
-                        let (rect, response) = ui.allocate_exact_size(
-                            egui::Vec2::new(32., 32.),
-                            Sense::hover().union(Sense::click()),
-                        );
-                        ui.child_ui(rect, *ui.layout()).add(|ui: &mut egui::Ui| {
-                            egui::Frame::none()
-                                .fill(egui::Color32::GRAY)
-                                .show(ui, |ui| recipe_icon(ui, &build.recipe, &icons))
-                                .response
-                        });
+                        let response = queue_item_ui(ui, build, &icons);
                         if response.clicked_by(PointerButton::Secondary) {
                             to_cancel.push(index);
                         }
@@ -51,4 +42,32 @@ pub fn crafting_queue_ui(
             crafting_queue.0.remove(*index);
         }
     }
+}
+
+fn queue_item_ui(
+    ui: &mut egui::Ui,
+    build: &mut crate::types::ActiveCraft,
+    icons: &Icons,
+) -> Response {
+    let (rect, response) = ui.allocate_exact_size(
+        egui::Vec2::new(32., 32.),
+        Sense::hover().union(Sense::click()),
+    );
+    if ui.is_rect_visible(rect) {
+        ui.child_ui(rect, *ui.layout()).add(|ui: &mut egui::Ui| {
+            egui::Frame::none()
+                .fill(egui::Color32::GRAY)
+                .show(ui, |ui| recipe_icon(ui, &build.recipe, icons))
+                .response
+        });
+
+        let progress_pct = build.timer.percent();
+        let rect = egui::Rect::from_min_size(
+            rect.min,
+            egui::Vec2::new(rect.width() * progress_pct, rect.height()),
+        );
+        ui.painter()
+            .rect_filled(rect, 0., Color32::from_black_alpha(200));
+    }
+    response
 }
